f2fbc6d91ca4015c41d1e50c7d992dd22d354eaf
MrDrawn <lipelimasilva123@gmail.com>
2022-04-21 16:59:16 -0300

    feat: add articles and category in home page

--- server.js
+++ server.js
@@ -19,8 +19,57 @@ server.use(express.static('public'));
 server.use(express.urlencoded({extended: false}));
 server.use(express.json());
 
-server.get('/', (request, response) => {
-  return response.render('index');
+server.get('/', async (request, response) => {
+  const articles = await Article.findAll({raw: true, order: [['id', 'desc']]});
+  const categories = await Category.findAll({raw: true, order: [['id', 'desc']]});
+
+  return response.render('index', {
+    articles,
+    categories,
+  });
+});
+
+server.get('/:slug', async (request, response) => {
+  const {slug} = request.params;
+
+  if (!slug) return response.redirect('/');
+
+  const article = await Article.findOne({
+    where: {
+      slug,
+    },
+  });
+
+  if (!article) return response.redirect('/');
+
+  const categories = await Category.findAll({raw: true, order: [['id', 'desc']]});
+
+  return response.render('article', {
+    article,
+    categories,
+  });
+});
+
+server.get('/category/:slug', async (request, response) => {
+  const {slug} = request.params;
+
+  if (!slug) return response.redirect('/');
+
+  const category = await Category.findOne({
+    where: {
+      slug,
+    },
+    include: [{model: Article}],
+  });
+
+  if (!category) return response.redirect('/');
+
+  const categories = await Category.findAll({raw: true, order: [['id', 'desc']]});
+
+  return response.render('index', {
+    articles: category.articles,
+    categories,
+  });
 });
 
 server.use('/', categoriesController);
