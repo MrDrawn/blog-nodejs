326cab0bf5143ad91e366042cf1c8e0c5e4c9279
MrDrawn <lipelimasilva123@gmail.com>
2022-04-21 15:40:41 -0300

    feat: add create article and tinymce

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (request, response) => {
-  return response.send('Rota de artigos');
+router.get('/admin/articles', (request, response) => {
+  return response.render('admin/articles/index');
+});
+
+router.get('/admin/articles/create', (request, response) => {
+  return response.render('admin/articles/create');
 });
 
 module.exports = router;
