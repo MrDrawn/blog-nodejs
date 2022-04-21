91cb1bae4e60066c8bef461a849355c5949d13f7
MrDrawn <lipelimasilva123@gmail.com>
2022-04-21 14:52:31 -0300

    feat: add delete category

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -4,8 +4,9 @@ const router = express.Router();
 const Category = require('./Category');
 
 const slugify = require('slugify');
+const {route} = require('express/lib/application');
 
-router.get('/categories', async (request, response) => {
+router.get('/admin/categories', async (request, response) => {
   const categories = await Category.findAll({raw: true});
 
   return response.render('admin/categories/index', {
@@ -27,7 +28,21 @@ router.post('/categories/save', async (request, response) => {
     slug: slugify(title),
   });
 
-  return response.redirect('/');
+  return response.redirect('/admin/categories');
+});
+
+router.get('/categories/delete/:id', async (request, response) => {
+  const {id} = request.params;
+
+  if (!id) return response.redirect('/admin/categories');
+
+  await Category.destroy({
+    where: {
+      id,
+    },
+  });
+
+  return response.redirect('/admin/categories');
 });
 
 module.exports = router;
